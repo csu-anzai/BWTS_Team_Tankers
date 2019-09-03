824a5c7ee5d659bdb50a2274d17136e812c2b052
cristina <Cristina12>
2019-09-03 12:06:42 +0200

    correct data

---
+++ combine_and_correct_data/correct_data.py
@@ -0,0 +1,24 @@
+import pandas as pd
+#Replace ; with :
+file_name = "E_2019_08_23__13_50_24.csv"
+def replace_to_comma(data):
+    text = open(data, "r")
+    text = ''.join([i for i in text]) \
+        .replace(";", ",")
+    x = open(file_name, "w")
+    x.writelines(text)
+    x.close()
+    drop_a()
+
+#Drop the empty column
+def drop_a():
+    file = pd.read_csv(file_name)
+
+    del file['a']
+
+    file.to_csv(file_name,index=False)
+
+replace_to_comma("../dataset/Team Tosca/E_2019_08_23__13_50_24.csv")
+
+
+#Add a team column
